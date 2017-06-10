e059da6e6e3258d1881dc12fb200fa9212fadff8
Zach Tuttle <zachary.tuttle@pearson.com>
2017-06-10 14:29:39 -0500

    modify the gitignore. moved configuration to its own file will a module added a bin directory

---
+++ overcast.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "Zach Tuttle"
+description   = "Personal cloud and data manager"
+license       = "MIT"
+
+# Dependencies
+
+requires "nim >= 0.17.0"
+
--- src/overcast_configuration.nim
+++ src/overcast_configuration.nim
@@ -2,13 +2,14 @@
 # Overcast has a configuration file that lives in the home directory. 
 # This configuration contains a list of directories to back up.
 #
-import os, parsecfg, streams
+import os, parsecfg
 
 const 
   overcastConfigurationDirectory: string = getHomeDir() & ".overcast"
   overcastConfigurationPathLocation: string = getHomeDir() & ".overcast/" & "backup_locations.ini"
 
-proc displayMessage(msg: string, msgType: string): void =
+
+proc displayMessage*(msg: string, msgType: string): void =
   # Message Types: SUCCESS, FAIL
   case msgType:
     of "SUCCESS":
@@ -18,19 +19,19 @@ proc displayMessage(msg: string, msgType: string): void =
     of "INFO":
       echo("[INFO] - " , msg)
 
-proc configurationDirectoryExists(): bool =
+proc configurationDirectoryExists*(): bool =
   if existsDir(overcastConfigurationDirectory):
     true
   else:
     false
 
-proc configurationFileExists(): bool =
+proc configurationFileExists*(): bool =
   if existsFile(overcastConfigurationPathLocation):
     true
   else:
     false
 
-proc createConfigurationDirectory(): void =
+proc createConfigurationDirectory*(): void =
   if not configurationDirectoryExists():
     createDir(overcastConfigurationDirectory)
     if configurationDirectoryExists():
@@ -38,7 +39,7 @@ proc createConfigurationDirectory(): void =
   else:
     displayMessage("configuration directory already exists at: " & overcastConfigurationDirectory , "INFO")
 
-proc createConfigurationFile(): void =
+proc createConfigurationFile*(): void =
   if configurationDirectoryExists():
     var overcastConfig = newConfig()
 
@@ -53,57 +54,31 @@ proc createConfigurationFile(): void =
   else:
     displayMessage("configuration directory doesn't exist", "FAIL")
 
-proc addDirectory(iKey: string, iDir: string): void =   
+proc addEntry*(iKey: string, iDir: string): bool =
+  var added = false
   if configurationDirectoryExists() and configurationFileExists():
     var config = loadConfig(overcastConfigurationPathLocation)
-
-    var file = newFileStream(overcastConfigurationPathLocation, fmRead)
     
-    if file != nil:
-      var parser: CfgParser
-      open(parser, file, overcastConfigurationDirectory)
-      
-      while true: 
-        var e = next(parser)
-
-        case e.kind
-          
-          of cfgEof:
-            displayMessage("backup_locations.ini has reached EOF!", "INFO")
-            break
-          
-          of cfgSectionStart:   ## a ``[section]`` has been parsed
-            if e.section != "BACKUP_DIRECTORIES":
-              break
-          
-          of cfgKeyValuePair:
-            # ensure that the key/dir doesn't already exist
-            if e.key == iKey and e.value == iDir:
-              echo "here"
-              displayMessage("KEY: " & iKey & "DIRECTORY: " & iDir & "already exists", "FAILURE")
-              break
-
-            ## check that the provided directory exists
-            if dirExists(iDir) == true:
-              config.setSectionKey("BACKUP_DIRECTORIES", iKey, iDir)
-              config.writeConfig(overcastConfigurationPathLocation)
-              displayMessage("\n\tAdded:\t\n\t\tKEY: " & iKey & "\n\t\tDIRECTORY: " & iDir, "SUCCESS")
-            else:
-              displayMessage("directory: " & iDir & "either doesn't exist or is not valid", "FAILURE")
-              break
-
-          of cfgOption:
-            echo("command: " & e.key & ": " & e.value)
-          
-          of cfgError:
-            displayMessage(e.msg, "FAILURE")
-      
-      close(parser)
-    
+    if dirExists(iDir):
+      config.setSectionKey("BACKUP_DIRECTORIES", iKey, iDir)
+      config.writeConfig(overcastConfigurationPathLocation)
+      added = true
     else:
-     echo("cannot open backup_locations.ini: " & overcastConfigurationPathLocation)
+      displayMessage(iDir & " is an invalid path", "FAIL")
 
-  else:
-    displayMessage("neither the configuration directory and configuration file exist...", "INFO")
+  added
+
+proc removeEntry*(iKey: string): bool =
+  var removed: bool = false
+  if configurationDirectoryExists() and configurationFileExists():
+    var config = loadConfig(overcastConfigurationPathLocation)
+    config.delSectionKey("BACKUP_DIRECTORIES", iKey)
+    config.writeConfig(overcastConfigurationPathLocation)
+    removed = true
+
+  removed
+
+
+
 
 
